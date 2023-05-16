0e4f1bf150b29c537f7512bfc83c1ccd85f943fe
Pato <placidomaidana@gmail.com>
2023-05-15 22:22:33 -0300

    Cambiamos algunas cositas

--- index.php
+++ index.php
@@ -5,13 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
 </head>
+
 <body>
     
-<form action="procesar_formulario.php" method="GET">
+<?php
+ /*if (!empty($_GET)) {
+    echo "Error". var_dump($_GET);
+ }*/
+if (!empty($_GET["error"]) && $_GET["error"]==1 ) {
+   
+    echo  '<div class="alert alert-primary" role="alert">
+    Corrija su nombre debe tener al menos tres caracteres....
+     </div>';
+    }
 
-Nombre:</br>
-<input type="text" name="nombre" id="nombre" minlength="3">
+    if (!empty($_GET["error"]) && $_GET["error"]==2 ) {
+   
+        echo  '<div class="alert alert-danger" role="alert">
+        El detalle no debe superar los diez caracteres...
+         </div>';
+        }
+    
+
+
+
+
+?>
+
+
+<form action="procesar_formulario.php" method="GET">
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+    Nombre:</br>
+<input type="text" name="nombre" id="nombre">
 </br>Email:</br>
 <input type="email" name="email" id="email" >
 </br>Contraseña:</br>
@@ -23,8 +53,34 @@ Nombre:</br>
     <option value="2">cebolla</option>
     <option value="3">acelga</option>
 </select></br>
+<br> Seleccione los generos de pelicula que le gusta:
+<br><input type="checkbox" name="Generos[]" value="acciòn" id=""> accion <br>
+<input type="checkbox" name="Generos[]" value="romantica">romantica <br>
+<input type="checkbox" name="Generos[]" value="comica"> comica <br>
+<input type="checkbox" name="Generos[]" value="terror">terror <br>
+
+    </div>
+    <div class="col">
+    Puede elejir un color a para su web: <br>
+<input type="radio" name="color" value="rojo">rojo<br>
+<input type="radio" name="color" value="verde">verde<br>
+<input type="radio" name="color" value="amarillo">amarillo<br>
+Escriba una breve descripcion:<br>
+<textarea name="detalle" id="" cols="20" rows="10"></textarea>
+
+
+
 
 <input type="submit" value="Enviar">
+
+    </div>
+ </div>
+</div>
+
+
+
+
+
 
 </form>
 
--- procesar_formulario.php
+++ procesar_formulario.php
@@ -3,7 +3,49 @@
 
 if (!empty($_GET)) {
     # code...
-    var_dump($_GET);
+
+    //var_dump($_REQUEST);die;
+    echo "Su gusto es exelente marco:";
+    foreach ($_REQUEST["Generos"] as $key => $value) {
+        # code...
+        if ($value=="romantica") {
+            # code...
+            echo "<br> Que dulce es usted señor";
+        }
+        echo $value."<br>"; 
+    }
+
+    if ($_REQUEST["color"]=="verde") {
+        # code...
+        echo "<br>Eligio un color ecologico<br>";
+    }
+
+
+    if ($_REQUEST["Verdura"]==1) {
+        # code...
+        echo "<br> Cuidado con eso, eso engorda <br>";
+    }
+
+    $nombre=$_GET["nombre"];
+   
+    if (StrLen($nombre)<3) {
+      
+        header('Location: http://localhost/UNNE_Programacion_III/formulario_unne/?error=1');
+        exit;
+   }
+  
+   if (strlen($_REQUEST["detalle"])>10) {
+    header('Location: http://localhost/UNNE_Programacion_III/formulario_unne/?error=2');
+        exit;
+   }
+  
+  
+  
+  
+   var_dump($_GET);
+
+   
+    
 }
 
 
